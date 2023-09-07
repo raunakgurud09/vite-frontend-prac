0520639dcf88ee9f54e3482fb3020e886edf5f95
Raunak Gurud <raunakgurud2121@gmail.com>
2023-09-07 17:09:53 +0530

    footer added

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,5 +1,50 @@
 import { Hexagon } from "lucide-react";
 
+const footer = [
+  {
+    heading: 'Socials',
+    options: [
+      {
+        url: 'https://github.com/raunakgurud09',
+        name: 'Github',
+      },
+      {
+        url: 'https://instagram.com/raunak_gurud',
+        name: 'Instagram',
+      },
+      {
+        url: 'https://github.com/raunakgurud09',
+        name: 'Linkedin',
+      },
+      {
+        url: 'https://github.com/raunakgurud09',
+        name: 'Twitter',
+      },
+      {
+        url: 'raunakgurud2121@gmail.com',
+        name: 'Mail',
+      },
+      {
+        url: 'https://raunakgurud.hashnode.dev',
+        name: 'Hashnode',
+      },
+    ],
+  },
+
+  {
+    heading: 'Random',
+    options: [
+      {
+        url: 'https://github.com/raunakgurud09',
+        name: 'Source code',
+      },
+      {
+        url: '/sitemap.xml',
+        name: 'Sitemap',
+      },
+    ],
+  },
+]
 
 export default function Footer() {
   return (
@@ -13,11 +58,27 @@ export default function Footer() {
             </div>
             <p className="text-[8px] absolute text-emerald-400 bottom-[-10px] right-0">Beta</p>
           </a>
-          <nav className="h-full">
-            <ul className="flex space-x-4 text-white/60 hover:text-white/80">
-              
-            </ul>
-          </nav>
+        </div>
+        <div className="flex space-x-5">
+          {footer.map(({ heading, options }) => (
+            <div className="w-fit" key={heading}>
+              <h4 className="font-bold text-md py-[2px]">{heading}</h4>
+              <ul className="text-xs">
+                {options.map((option) => (
+                  <a
+                    href={option.url}
+                    key={option.name}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="hover:opacity-100 opacity-70 text-white/60 hover:text-white/80"
+                    aria-label={option.name}
+                  >
+                    <li className="py-[2px]">{option.name}</li>
+                  </a>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </footer>
